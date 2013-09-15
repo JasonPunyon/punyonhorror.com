63abf490b720718eb0fa292422c32582f55b2d4c
Jason Punyon <jason.punyon@gmail.com>
2013-09-15 08:34:13 -0400

    Don't specify a listening address.

--- mentor.js
+++ mentor.js
@@ -44,6 +44,6 @@ var port = process.env.PORT || 1337;
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end(getMentorism());
-}).listen(port, '127.0.0.1');
+}).listen(port);
 
 console.log('Get your mentor on at http://localhost:' + port);
